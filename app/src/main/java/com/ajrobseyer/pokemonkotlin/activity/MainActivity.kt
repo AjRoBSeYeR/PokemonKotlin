bdd9879a92d2b27fb646b99c9dfe60892ea6b329
Borja <borja.reyes@vincoorbis.com>
2020-03-27 17:45:32 -0600

    traslado de llamada a fragment

--- app/src/main/java/com/ajrobseyer/pokemonkotlin/activity/MainActivity.kt
+++ app/src/main/java/com/ajrobseyer/pokemonkotlin/activity/MainActivity.kt
@@ -3,11 +3,7 @@ package com.ajrobseyer.pokemonkotlin.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ajrobseyer.pokemonkotlin.R
-import com.ajrobseyer.pokemonkotlin.model.PokemonResponse
-import com.ajrobseyer.pokemonkotlin.util.RestClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,23 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val apiService =
-            RestClient.getRestClient()
-
-        apiService.get20Pokemon().enqueue(object : Callback<PokemonResponse> {
-            override fun onResponse(
-                call: Call<PokemonResponse>?,
-                response: Response<PokemonResponse>?
-            ) {
-                response?.body().let {
-                    lateinit var bundle: Bundle
-                    bundle.putSerializable("pokemonList", response?.body())
-                }
-            }
-
-            override fun onFailure(call: Call<PokemonResponse>?, t: Throwable?) {
-                t?.printStackTrace()
-            }
-        })
     }
+
+
 }
--- app/src/main/java/com/ajrobseyer/pokemonkotlin/fragment/HeaderFragment.kt
+++ app/src/main/java/com/ajrobseyer/pokemonkotlin/fragment/HeaderFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.ajrobseyer.pokemonkotlin.R
+import com.ajrobseyer.pokemonkotlin.model.PokemonResponse
+import com.ajrobseyer.pokemonkotlin.util.RestClient
+import kotlinx.android.synthetic.main.fragment_header.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 class HeaderFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private var param1: String? = ""
+
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String) =
+            HeaderFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                }
+                return HeaderFragment()
+            }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -25,18 +44,36 @@ class HeaderFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_header, container, false)
+
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HeaderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val apiService =
+            RestClient.getRestClient()
+
+        apiService.get20Pokemon().enqueue(object : Callback<PokemonResponse> {
+            override fun onResponse(
+                call: Call<PokemonResponse>?,
+                response: Response<PokemonResponse>?
+            ) {
+                response?.body().let {
+                    var currentCount = 0
+                    var totalCount = 0
+                    val data: PokemonResponse? =response!!.body()
+                    tvCounter.text = "$currentCount / $totalCount"
+
+                   //val fragment = HeaderFragment.newInstance("Pedro")
+                   //supportFragmentManager.beginTransaction().replace(R.id.headFragment, fragment).commit()
                 }
             }
+
+            override fun onFailure(call: Call<PokemonResponse>?, t: Throwable?) {
+                t?.printStackTrace()
+            }
+        })
+
     }
 }
