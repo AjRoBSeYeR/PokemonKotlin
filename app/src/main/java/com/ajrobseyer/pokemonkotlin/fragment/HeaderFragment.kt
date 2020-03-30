b54d1ed7ad4b38f7ee679ad101605f98cb2669a8
Borja <borja.reyes@vincoorbis.com>
2020-03-30 12:30:28 -0600

    visibilidad boton previous

--- app/src/main/java/com/ajrobseyer/pokemonkotlin/fragment/HeaderFragment.kt
+++ app/src/main/java/com/ajrobseyer/pokemonkotlin/fragment/HeaderFragment.kt
@@ -5,18 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.ajrobseyer.pokemonkotlin.R
 import kotlinx.android.synthetic.main.fragment_header.*
+import org.jetbrains.anko.support.v4.toast
 
 
 class HeaderFragment : Fragment() {
-    private var countIni: String? = "0"
-    private var countTotal: String? = "0"
+    private var countIni: String = "0"
+    private var countTotal: String = "0"
 
 
     companion object {
         @JvmStatic
-        fun newInstance(ini: String, total:String) =
+        fun newInstance(ini: String, total: String) =
             HeaderFragment().apply {
                 arguments = Bundle().apply {
                     putString("countIni", ini)
@@ -28,8 +30,8 @@ class HeaderFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            countIni = it.getString("countIni")
-            countTotal=it.getString("countTotal")
+            countIni = it.getString("countIni").toString()
+            countTotal = it.getString("countTotal").toString()
         }
     }
 
@@ -44,8 +46,19 @@ class HeaderFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val spacePosition = countIni.indexOf(" ")
+        val upperLimit = countIni.substring(0, spacePosition)
+        if (upperLimit.toInt() == 1) {
+            btnPrev.visibility = View.INVISIBLE
+        }else{
+            btnPrev.visibility = View.VISIBLE
+        }
 
         tvCounter.text = "$countIni de $countTotal"
+
+        btnPrev.setOnClickListener {
+
+        }
 
     }
 }
